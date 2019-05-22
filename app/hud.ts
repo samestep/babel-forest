7d4ea19bd81da99c581b3a7512acd4f18b3008da
Sam Estep <sam@samestep.com>
2019-05-22 13:57:55 -0400

    Count runs in localStorage

--- app/hud.ts
+++ app/hud.ts
@@ -4,7 +4,10 @@ export class HUD {
   text: Phaser.GameObjects.Text;
 
   constructor(scene: Phaser.Scene) {
-    this.text = scene.add.text(0, 0, 'Hello, world!', { fontFamily: 'sans' });
+    const worldStr = localStorage.getItem('babel-forest');
+    const worldNum = worldStr ? parseInt(worldStr, 10) + 1 : 0;
+    localStorage.setItem('babel-forest', worldNum.toString());
+    this.text = scene.add.text(0, 0, `Hello, world #${worldNum}!`, { fontFamily: 'sans' });
     this.text.setScrollFactor(0, 0);
   }
 }
