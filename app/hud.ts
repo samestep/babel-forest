b5bcca01d8c043716364d96c2d1e00aab33c1438
Sam Estep <sam@samestep.com>
2019-05-23 10:02:46 -0400

    Replace the localStorage conflict with text reveal

--- app/hud.ts
+++ app/hud.ts
@@ -1,14 +1,33 @@
 /// <reference path="../node_modules/phaser/types/phaser.d.ts" />
 
+const delay = 100;
+
 export class HUD extends Phaser.Scene {
+  cooldown: number;
+  text: Phaser.GameObjects.Text;
+  letters: string[];
+  running: string;
+
   create() {
     addEventListener('resize', () => {
       this.cameras.main.setSize(innerWidth, innerHeight);
     });
 
-    const worldStr = localStorage.getItem('babel-forest');
-    const worldNum = worldStr ? parseInt(worldStr, 10) + 1 : 0;
-    localStorage.setItem('babel-forest', worldNum.toString());
-    this.add.text(0, 0, `Hello, world #${worldNum}!`, { fontFamily: 'sans' });
+    this.running = '';
+    this.text = this.add.text(0, 0, this.running, { fontFamily: 'sans' });
+    this.cooldown = delay;
+    this.letters = 'Hello, world!'.split('');
+  }
+
+  update(time: number, delta: number) {
+    this.cooldown -= delta;
+    if (this.cooldown <= 0) {
+      this.cooldown = delay;
+      if (this.letters.length > 0) {
+        this.running += this.letters[0];
+        this.letters.shift();
+        this.text.setText(this.running);
+      }
+    }
   }
 }
